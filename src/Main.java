c42eaeef9489a244b91cda46870825e9111ad2a5
vlad <vladislav.nesterov1990@mail.ru>
2023-02-20 12:45:48 +0300

    Первый коммит()

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Будем делать так");
         System.out.println("Вот так ");
+        System.out.println("Сделать Pull-request из одной ветки в другую аа ");
     }
 
 }
